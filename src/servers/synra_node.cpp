b4b6629a116afd1a0836194cd716e13af2709d3f
Lucas <lucasphone75@gmail.com>
2026-03-21 00:34:25 -0600

    Revert "add log recovery"
    This reverts commit a3e91b14011085c5b2046e85e16a09d287d757f6.

--- src/servers/synra_node.cpp
+++ src/servers/synra_node.cpp
@@ -822,12 +822,8 @@ void SynraNode::run() {
             }
         }
 
-        if (exit_deadline.has_value()) {
-            if (std::chrono::steady_clock::now() >= *exit_deadline) {
-                return;
-            }
-            std::this_thread::sleep_for(std::chrono::milliseconds(10));
-            continue;
+        if (exit_deadline.has_value() && std::chrono::steady_clock::now() >= *exit_deadline) {
+            return;
         }
 
         const int n = ibv_poll_cq(cq_, 64, wc);
