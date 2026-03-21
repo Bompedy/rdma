e6c67f4901f68909b940c9055dba6377d90042a1
Lucas <lucasphone75@gmail.com>
2026-03-21 00:37:41 -0600

    Reapply "add log recovery"
    This reverts commit 292fc87ba26e18bc7ac7a151900cfe7266f7f320.

--- src/servers/synra_node.cpp
+++ src/servers/synra_node.cpp
@@ -834,6 +834,9 @@ void SynraNode::run() {
         for (int i = 0; i < n; ++i) {
             const ibv_wc& comp = wc[i];
             if ((comp.opcode & IBV_WC_RECV) != 0) {
+                if (comp.status == IBV_WC_WR_FLUSH_ERR && exit_deadline.has_value()) {
+                    continue;
+                }
                 RecoveryControlMessage msg{};
                 bool from_client = false;
                 uint32_t sender_id = 0;
@@ -855,6 +858,9 @@ void SynraNode::run() {
                 continue;
             }
 
+            if (comp.status == IBV_WC_WR_FLUSH_ERR && exit_deadline.has_value()) {
+                continue;
+            }
             if (comp.status != IBV_WC_SUCCESS) {
                 std::cerr << "[SynraNode] WC error: "
                           << ibv_wc_status_str(comp.status)
